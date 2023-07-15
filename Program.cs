fd52c566f811c04d7dba12b86735c42d8e158e8f
Backinblacker <coachblacker@gmail.com>
2023-07-15 14:00:24 -0600

    📦 NEW: Playing with Const and variables

--- Program.cs
+++ Program.cs
@@ -8,11 +8,17 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("What is your name?");
-            var name = System.Console.ReadLine();
-            var currentDate = System.DateTime.Now;
-            System.Console.WriteLine($"{System.Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            System.Console.Write($"{System.Environment.NewLine}Press any key to exit...");
-            System.Console.ReadKey(true);
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.WriteLine($"{name} is ready to learn C#");
+            const int num1 = 17;
+            int num2 = 32;
+            Console.WriteLine(num1+num2);
+
+            // write code above this line in order to not have to start over (Practice Comment)
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
