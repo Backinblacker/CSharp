236ce38b73d2bd1c6259cfb92559cd93b66cdd6f
Backinblacker <coachblacker@gmail.com>
2023-07-15 13:44:16 -0600

    📦 NEW: First function What's your name?

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace HelloWorld
+﻿using System;
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("What is your name?");
+            var name = System.Console.ReadLine();
+            var currentDate = System.DateTime.Now;
+            System.Console.WriteLine($"{System.Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            System.Console.Write($"{System.Environment.NewLine}Press any key to exit...");
+            System.Console.ReadKey(true);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CSharp
 Learning C#
+
+Starting 15Jul23 I am Learning how to use C# in VSCode. 
+Can't wait to see where this goes.
+If you have any fun ideas, send me a message.
